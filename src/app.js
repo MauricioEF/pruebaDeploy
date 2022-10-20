54dd015deeb1844088fcf6647734c86d4f113e75
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-10-20 08:29:59 -0500

    Added PORT

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
 const app = express();
-
-app.listen(8080,()=>console.log(`Listening on PORT 8080`))
+const PORT = process.env.PORT||8080
+app.listen(PORT,()=>console.log(`Listening on PORT ${PORT}`))
 
 app.get('/',(req,res)=>{
     res.send({status:"success",message:"listo"})
